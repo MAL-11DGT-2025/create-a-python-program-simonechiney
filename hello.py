92f97ab3469ba1248dba27a2f94a0471f45ccd1d
simone.chiney <simone.cadampog@marcellin.school.nz>
2025-03-04 10:52:02 +1300

    created another code

--- hello.py
+++ hello.py
@@ -3,4 +3,8 @@ print("My favourite color is yellow.")
 name = input("What is your name? ")
 print(f'Hello {name}!')
 age = input("How old are you? ")
-print(f'{name} You look quite young for your age')
+print(f'{name}, You look quite young for your age.')
+
+if age > 25: 
+    print("You are old.")
+    
