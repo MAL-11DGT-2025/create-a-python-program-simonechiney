35cc7631d0b9daefec41b8f39582b3797f6c2a67
simone.chiney <simone.cadampog@marcellin.school.nz>
2025-03-04 10:33:29 +1300

    created a program that asked user for name and age

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
 print("Hello, World!")
 print("My favourite color is yellow.")
+name = input("What is your name? ")
+print(f'Hello {name}!')
+age = input("How old are you? ")
+print(f'{name} You look quite young for your age')
