deded3d55fa85e1846aa0da95c6183193a9660cd
simone.chiney <simone.cadampog@marcellin.school.nz>
2025-03-06 14:43:51 +1300

    Added code that asked for age and commented on code

--- hello.py
+++ hello.py
@@ -1,15 +1,29 @@
-print("Hello, World!")
-print("My favourite color is yellow.")
+# print("Hello, World!")
+# print("My favourite color is yellow.")
 
-name = input("What is your name? ")
-print(f'Hello {name}!')
+# name = input("What is your name? ")
+# print(f'Hello {name}!')
 
-age = int(input("How old are you? "))
-print(f'{name}, You look quite young for your age.')
+# age = int(input("How old are you? "))
+# print(f'{name}, You look quite young for your age.')
 
-if age > 25:
-    print("You are old.")
+# if age > 25:
+   # print("You are old.")
 
-if age >= 13 and age <= 19:
-    print("You are a teenager.")
+# if age >= 13 and age <= 19:
+   # print("You are a teenager.")
     
+# if age >= 13 or age <= 19:
+    #print("You might be a teenager.")
+    
+# umbrella = True 
+# sunny = False
+
+# if umbrella and sunny:
+    print("You think it's going to rain.")
+# elif not umbrella and sunny:
+   # print("You think you don't need an umbrella.")
+# elif umbrella and not sunny:
+   # print("It must be raining.")
+#elif not umbrella and not sunny:
+    #print("You must be drenched.")
