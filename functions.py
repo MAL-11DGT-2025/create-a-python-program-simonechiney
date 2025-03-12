cb3b6c6f72a68a7c6ecc28ae1c5e41da2596dfe3
simone.chiney <simone.cadampog@marcellin.school.nz>
2025-03-13 11:04:58 +1300

    Created a program that allows user to choose an operation

---
+++ functions.py
@@ -0,0 +1,41 @@
+def greeting():
+    print("Mabuhay!")
+
+def greeting_w_name(name):
+    print(f"Mabuhay {name}!")
+
+ #def add(a, b):
+    # print(a + b)
+
+# greeting ()
+# greeting_w_name("Simone") 10)
+# greeting_w_name("Peter")
+# greeting_w_name("Paul")
+# add(5,50, 2)
+# add(887, 233)
+
+ #def subtract(a, b):
+   #  print(a - b)
+# subtract(60, 4)
+#subtract(87, 65)
+
+#def divide(a, b):
+    #print(a / b)
+#divide(20, 2)
+#divide(87, 3)
+
+#  multiply(a, b):
+   # print(a * b)
+#multiply(2387238, 2334)
+#multiply(900, 3)
+
+operation = input("Which operation do you want to use (+, -, *, /): ")
+a = input("Enter your first number: ")
+b = input("Enter your second number: ")
+
+def add(a, b):
+
+if operation == "+":
+    add(a, b)
+elif operation == "-":
+    subtract(a, b)
